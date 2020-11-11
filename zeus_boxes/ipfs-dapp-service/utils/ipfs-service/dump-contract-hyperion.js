6f82bbcfb4f48643460a8bb9a93b69dc05772b47
cmichel21 <cmichel_zmeta@protonmail.com>
2020-11-11 16:13:19 +0000

    improve replay-dump script (print ipfs hashes)

--- zeus_boxes/ipfs-dapp-service/utils/ipfs-service/dump-contract-hyperion.js
+++ zeus_boxes/ipfs-dapp-service/utils/ipfs-service/dump-contract-hyperion.js
@@ -3,12 +3,28 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const { Serialize } = require("eosjs");
 const querystring = require("querystring");
+const multihash = require('multihashes');
+const sha256 = require('js-sha256').sha256;
 
 const contractAccount = process.env.CONTRACT || "";
 
 const hyperionEndpoint =
   process.env.HYPERION_ENDPOINT || "https://api-wax.maltablock.org";
 if (!contractAccount) throw new Error(`must set CONTRACT`);
+
+const hashData256 = (data) => {
+    var hash = sha256.create();
+    hash.update(data);
+    return hash.hex();
+};
+// zeus_boxes/storage-dapp-service/services/storage-dapp-service-node/common.js
+const dataToIpfsHash = (dataBuffer) => {
+    const hash = hashData256(dataBuffer);
+    const paddedHash = "01551220" + hash;
+    const bytes = Buffer.from(paddedHash, 'hex');
+    const address = multihash.toB58String(bytes);
+    return 'ipfs://z' + address;
+}
 
 async function run() {
   let skip = 0;
@@ -29,7 +45,11 @@ async function run() {
     const commits = response.deltas;
     skip += commits.length;
     allCommits.push(
-      ...commits.map((c) => ({ timestamp: c.timestamp, data: c.data.data }))
+      ...commits.map((c) => ({
+          timestamp: c.timestamp,
+          data: Buffer.from(c.data.data, `hex`).toString(`base64`),
+          hash: dataToIpfsHash(Buffer.from(c.data.data, `hex`))
+        }))
     );
 
     if (commits.length === 0) break;
